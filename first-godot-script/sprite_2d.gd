ff642219b9673e7389fc2f421fb036c23255be3c
Anpilov Andrey <62002015+AnpilovAA@users.noreply.github.com>
2026-04-10 18:28:20 +0300

    Разбирал сигналы, и как сделать кастомный. пока не всё понял

--- first-godot-script/sprite_2d.gd
+++ first-godot-script/sprite_2d.gd
@@ -3,17 +3,22 @@ extends Sprite2D
 var speed = 400
 var angular_speed = PI
 
-func _process(delta: float) -> void:
-	var direction = 0
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-	if Input.is_action_pressed("ui_right"):
-		direction = 1
+func _ready() -> void:
+	var time = get_node("Timer")
+	time.timeout.connect(_on_timer_timeout)
 
-	rotation += angular_speed * direction * delta
-	
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP.rotated(rotation) * speed
-	
+func _process(delta: float) -> void:
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
+
+
+func _on_button_toggled(toggled_on: bool) -> void:
+	set_process(not is_processing())
+
+func _on_button_pressed() -> void:
+	set_process(not is_processing())
+
+	
+func _on_timer_timeout():
+	visible = not visible
